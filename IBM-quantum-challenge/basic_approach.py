b14110bca178caa23bab0b15128b4da43fd4feab
Linueks <linueks@gmail.com>
2022-01-13 14:57:09 +0100

    working with noiseless simulator now. Getting better result with more trotter steps as expected

--- IBM-quantum-challenge/basic_approach.py
+++ IBM-quantum-challenge/basic_approach.py
@@ -3,9 +3,10 @@ Starting with implementation provided by IBM at:
 https://github.com/qiskit-community/open-science-prize-2021/blob/main/ibmq-qsim-challenge.ipynb
 """
 import numpy as np
-import matplotlib.pyplot as plt
 import qiskit as qk
+import time as time
 import qiskit.opflow as opflow
+import matplotlib.pyplot as plt
 import qiskit.ignis.verification.tomography as tomo
 from qiskit.providers.aer import QasmSimulator
 from qiskit.quantum_info import state_fidelity
@@ -17,13 +18,9 @@ warnings.filterwarnings('ignore')
 
 
 
-
-
 def heisenberg_chain():
     # define Heisenberg XXX hamiltonian opflow way
-    # defining qubit states and operators using qiskit opflow
-    ket_zero = opflow.Zero
-    ket_one = opflow.One
+    # defining operators using qiskit opflow
     identity = opflow.I
     pauli_x = opflow.X
     pauli_y = opflow.Y
@@ -47,11 +44,10 @@ def propagator(time):
 
 
 
-def classical_simulation():
+def classical_simulation(initial_state):
     # A copy paste from the notebook just to have it here
 
     time_points = np.linspace(0, np.pi, 100)
-    initial_state = ket_one^ket_one^ket_zero
     probability_110 = np.zeros_like(time_points)
 
     for i, t in enumerate(time_points):
@@ -66,7 +62,7 @@ def classical_simulation():
 
 
 
-def construct_trotter_gate(time):
+def construct_trotter_gate(t):
     # decomposition of propagator into quantum gates (copy pasta)
     # I'm not sure I like this way of programming using opflow. Feels like I don't
     # know what's happening behind the scenes. Ask Alessandro
@@ -81,18 +77,18 @@ def construct_trotter_gate(time):
 
     xx_circuit.ry(np.pi/2, [0,1])
     xx_circuit.cnot(0, 1)
-    xx_circuit.rz(2*time, 1)
+    xx_circuit.rz(2*t, 1)
     xx_circuit.cnot(0, 1)
     xx_circuit.ry(-np.pi/2, [0,1])
 
     yy_circuit.rx(np.pi/2, [0,1])
     yy_circuit.cnot(0, 1)
-    yy_circuit.rz(2*time, 1)
+    yy_circuit.rz(2*t, 1)
     yy_circuit.cnot(0, 1)
     yy_circuit.rx(-np.pi/2, [0,1])
 
     zz_circuit.cnot(0, 1)
-    zz_circuit.rz(2*time, 1)
+    zz_circuit.rz(2*t, 1)
     zz_circuit.cnot(0, 1)
 
     # Convert custom quantum circuit into a gate
@@ -118,9 +114,9 @@ def construct_trotter_gate(time):
 
 
 
-def trotterized_simulation(time,
+def trotterized_simulation(t,
                            target_time=np.pi,
-                           trotter_steps=4,
+                           trotter_steps=16,
                            draw_circuit=False):
     # generate the full circuit for the trotterized simulation
     # there are also some "fancy / ugly" things happening here
@@ -129,7 +125,7 @@ def trotterized_simulation(time,
 
     # set up initial state |110>
     quantum_circuit.x([3, 5])
-    single_trotter_step = construct_trotter_gate(time)
+    single_trotter_step = construct_trotter_gate(t)
 
     for n in range(trotter_steps):
         quantum_circuit.append(single_trotter_step,
@@ -138,7 +134,7 @@ def trotterized_simulation(time,
                              quantum_register[5]])
 
         quantum_circuit = quantum_circuit.bind_parameters(
-                            {time: target_time/trotter_steps})
+                            {t: target_time/trotter_steps})
         final_circuit = tomo.state_tomography_circuits(quantum_circuit,
                                                     [quantum_register[1],
                                                      quantum_register[3],
@@ -152,17 +148,48 @@ def trotterized_simulation(time,
 
 
 
-def execute_circuit(shots=8192, repetitions=8, backend=):
-    # wrapper function to run jobs
+def execute_circuit(circuit,
+                    shots=8192,
+                    repetitions=8,
+                    backend=QasmSimulator()):
+    # wrapper function to run jobs. Assumes QasmSimulator is imported as such.
+    jobs = []
+    for i in range(repetitions):
+        job = qk.execute(circuit, backend, shots=shots)
+        print(f'Job ID: {job.job_id()}')
+        jobs.append(job)
+
+    for job in jobs:
+        qk.tools.monitor.job_monitor(job)
+
+        # this thing seems stupid too
+        try:
+            if job.error_message() is not None:
+                print(job.error_message())
+        except:
+            pass
+
+    return jobs
 
 
 
+def tomography_analysis(result, circuit, target_state):
+    # assumes the target state is given as a qiskit opflow state
+    target_state_matrix = target_state.to_matrix()
+    tomography_fitter = tomo.StateTomographyFitter(result, circuit)
+    rho_fit = tomography_fitter.fit(method='lstsq')
+    fidelity = qk.quantum_info.state_fidelity(rho_fit, target_state_matrix)
 
+    return fidelity
 
 
 
 if __name__=='__main__':
-    #classical_simulation()
+    ket_zero = opflow.Zero
+    ket_one = opflow.One
+    initial_state = ket_one^ket_one^ket_zero
+
+    #classical_simulation(initial_state)
     time = qk.circuit.Parameter('t')
 
     # set up qiskit simulators
@@ -174,6 +201,13 @@ if __name__=='__main__':
     jakarta_noisy = QasmSimulator.from_backend(provider.get_backend('ibmq_jakarta'))
     """
 
+    circuit = trotterized_simulation(time, draw_circuit=False)
+    jobs = execute_circuit(circuit, backend=jakarta_noiseless)
+    fidelities = []
 
+    for job in jobs:
+        fidelity = tomography_analysis(job.result(), circuit, initial_state)
+        fidelities.append(fidelity)
 
-    trotterized_simulation(time, draw_circuit=True)
+    print(f'state tomography fidelity = {np.mean(fidelities):.4f}',
+           '\u00B1', f'{np.std(fidelities):.4f}')
